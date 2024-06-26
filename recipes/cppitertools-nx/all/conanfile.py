c9e682ca3ad0d7443c7d39b0b8ea604777059ea7
user <user@localhost>
2024-06-26 13:44:57 +0300

    FIX: cppitertools-nx now conan-2

--- recipes/cppitertools-nx/all/conanfile.py
+++ recipes/cppitertools-nx/all/conanfile.py
@@ -1,8 +1,7 @@
-import os
-from conans import ConanFile, tools
+import os, conan
+from conan import ConanFile, tools
 
 required_conan_version = ">=1.32.0"
-
 
 class CppItertoolsConan(ConanFile):
     name = "cppitertools-nx"
@@ -11,26 +10,21 @@ class CppItertoolsConan(ConanFile):
     description = "Implementation of python itertools and builtin iteration functions for C++17. With some home brew iterators"
     topics = ("cpp17", "iter", "itertools")
     license = "BSD-2-Clause"
+    package_type = "header-library"
     no_copy_source = True
 
     settings = "os", "arch", "compiler", "build_type"
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = "cppitertools-" + self.version + ".0-nx"
-        os.rename(extracted_dir, self._source_subfolder)
+        tools.files.get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def package(self):
-        self.copy("*.hpp", dst=os.path.join("include", "cppitertools"), src=self._source_subfolder, excludes=('examples/**', 'test/**'))
-        self.copy("LICENSE.md", dst="licenses", src=self._source_subfolder)
+        tools.files.copy(self, "*.hpp", 
+                         src=self.source_folder,
+                         dst=os.path.join(self.package_folder, "include", "cppitertools"),
+                         excludes=('examples/**', 'test/**'))
+        tools.files.copy(self, "LICENSE.md", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "cppitertools"
         self.cpp_info.names["cmake_find_package_multi"] = "cppitertools"
-
-    def package_id(self):
-        self.info.header_only()
--- recipes/cppitertools-nx/all/test_package/conanfile.py
+++ recipes/cppitertools-nx/all/test_package/conanfile.py
@@ -1,17 +1,37 @@
 import os
-from conans import ConanFile, CMake, tools
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain, CMakeDeps
+from conan.tools.build import can_run
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package_multi"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
+    def layout(self):
+        cmake_layout(self)
+
     def test(self):
-        if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(cmd, env="conanrun")
+            # sense if it was built because we don't have access to:
+            # self.dependencies["cppitertools"].options.zip_longest
+            cmd = os.path.join(self.cpp.build.bindir, "test_zip_longest")
+            if os.path.exists(cmd):
+                self.run(cmd, env="conanrun")
