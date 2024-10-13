23b55e2a8cf0e8e97750a9cf867d9ad0b686d686
hoxnox <hoxnox@gmail.com>
2024-10-13 10:32:54 +0300

    ENH: mathgl added

---
+++ recipes/mathgl/all/conanfile.py
@@ -0,0 +1,164 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
+from conan.tools.files import get,copy
+import shutil
+import os
+
+class MathglConan(ConanFile):
+    name = "mathgl"
+    version = "8.0.2"
+    license = "LGPL-3.0-only | GPL-3.0-only"
+    url = "https://github.com/hoxnox/conan-mathgl"
+    author = "Alexey Balakin <balakin@appl.sci-nnov.ru>"
+    homepage = "http://mathgl.sourceforge.net"
+    description = "MathGL is a library for making high-quality scientific graphics under Linux and Windows."
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "lgpl": [True, False],
+        "double_precision": [True, False],
+        "rvalue_support": [True, False],
+        "pthread": [True, False],
+        "pthr_widget": [True, False],
+        "openmp": [True, False],
+        "opengl": [True, False],
+        "glut": [True, False],
+        "fltk": [True, False],
+        "wxWidgets": [True, False],
+        "qt5": [True, False],
+        "zlib": [True, False],
+        "png": [True, False],
+        "jpeg": [True, False],
+        "gif": [True, False],
+        "pdf": [True, False],
+        "gsl": [True, False],
+        "hdf5": [True, False],
+        "mpi": [True, False],
+        "ltdl": [True, False],
+        "all_swig": [True, False]
+    }
+    default_options = {
+            "lgpl": True,
+            "double_precision": True,
+            "rvalue_support": False,
+            "pthread": False,
+            "pthr_widget": False,
+            "openmp": True,
+            "opengl": True,
+            "glut": False,
+            "fltk": False,
+            "wxWidgets": False,
+            "qt5": False,
+            "zlib": True,
+            "png": True,
+            "jpeg": True,
+            "gif": False,
+            "pdf": False,
+            "gsl": False,
+            "hdf5": False,
+            "mpi": False,
+            "ltdl": False,
+            "all_swig": False
+    }
+    cmake_options = {}
+
+    def add_cmake_opt(self, val, doAdd):
+        if doAdd:
+            self.cmake_options["enable-{}".format(val)] = "ON"
+        else:
+            self.cmake_options["enable-{}".format(val)] = "OFF"
+
+    def requirements(self):
+
+        self.add_cmake_opt("double", self.options.double_precision)
+        self.add_cmake_opt("mpi", self.options.mpi)
+        self.add_cmake_opt("opengl", self.options.opengl)
+        self.add_cmake_opt("rvalue", self.options.rvalue_support)
+        self.add_cmake_opt(
+            "pthread", self.options.pthread
+        )  # Either enable pthread of openmp but not both at the same time
+        self.add_cmake_opt("openmp", self.options.openmp)
+        self.add_cmake_opt("ltdl", self.options.ltdl)
+
+        self.add_cmake_opt("lgpl", self.options.lgpl)
+        self.add_cmake_opt("pthr-widget",
+                           self.options.pthr_widget)  # pthread widget
+        self.add_cmake_opt("glut", self.options.glut)
+        self.add_cmake_opt("fltk", self.options.fltk)
+        self.add_cmake_opt("wx", self.options.wxWidgets)
+        self.add_cmake_opt("qt5", self.options.qt5)
+        self.add_cmake_opt("zlib", self.options.zlib)
+        self.add_cmake_opt("png", self.options.png)
+        self.add_cmake_opt("jpeg", self.options.jpeg)
+        self.add_cmake_opt("gif", self.options.gif)
+        self.add_cmake_opt("pdf", self.options.pdf)
+        self.add_cmake_opt("mpi", self.options.mpi)
+        self.add_cmake_opt("ltdl", self.options.ltdl)
+        if not self.options.lgpl:
+            self.add_cmake_opt("gsl", self.options.gsl)
+            self.add_cmake_opt("hdf5", self.options.hdf5)
+            self.add_cmake_opt("all-swig", self.options.all_swig)
+
+        # expected to be found w/o conan: opengl, glut, fltk, wxwidgets, mpi, ltdl, gsl, qt
+        if self.options.zlib:
+            self.requires("zlib/[>=1.2.11]")
+            # self.options["zlib"].shared = False
+        if self.options.png:
+            self.requires("libpng/[>=1.6.34]")
+            # self.options["libpng"].shared = False
+        if self.options.jpeg:
+            self.requires("libjpeg-turbo/[>=1.5.2]")
+            # self.options["libjpeg-turbo"].shared = False
+            # set jpeg version 62
+        if self.options.gif:
+            self.requires("giflib/[>=5.1.3]")
+            # self.options["giflib"].shared = False
+        if self.options.pdf:
+            self.requires("libharu/2.3.0")
+            # self.options["libharu"].shared = False
+        if self.options.hdf5:
+            if not self.options.lgpl:
+                self.requires("hdf5/[>=1.10.5]")
+                # self.options["HDF5"].shared = False
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+       deps = CMakeDeps(self)
+       deps.generate()
+       tc = CMakeToolchain(self)
+       for k, v in self.cmake_options.items():
+           tc.variables[k] = v
+       tc.variables["SOME_TEST_VAR"] = True
+       tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+
+        self.cpp_info.libs = ["mgl"]
+        if self.options.fltk:
+            self.cpp_info.libs.append('mgl-fltk')
+        if self.options.glut:
+            self.cpp_info.libs.append('mgl-glut')
+        if self.options.qt5:
+            self.cpp_info.libs.append('mgl-qt5')
+            self.cpp_info.libs.append('mgl-qt')
+            if self.options.fltk:
+                self.cpp_info.libs.append('mgl-wnd')
+        if self.options.wxWidgets:
+            self.cpp_info.libs.append('mgl-wx')
+
+        # if not self.options.shared and self.settings.compiler == "Visual Studio":
+        #     for lib in range(len(self.cpp_info.libs)):
+        #         self.cpp_info.libs[lib] += "-static"
+        # if self.settings.build_type == "Debug" and self.settings.compiler == "Visual Studio":
+        #     for lib in range(len(self.cpp_info.libs)):
+        #         self.cpp_info.libs[lib] += "d"
---
+++ recipes/mathgl/all/test_package/conanfile.py
@@ -0,0 +1,36 @@
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain, CMakeDeps
+from conan.tools.build import can_run
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    test_type = "explicit"
+    default_options = {"mathgl/*:jpeg": True}
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def layout(self):
+        cmake_layout(self)
+
+    def test(self):
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(cmd, env="conanrun")
+            if os.path.exists(cmd):
+                self.run(cmd, env="conanrun")
+
