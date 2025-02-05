4c17de42a87f0e2c0b297c3f1c0dd095ee76814f
hoxnox <i@hoxnox.com>
2025-02-05 14:26:58 +0300

    ENH: libxdp customXdpProg option added

--- recipes/libxdp/all/conanfile.py
+++ recipes/libxdp/all/conanfile.py
@@ -5,6 +5,7 @@ from conan.tools.files import copy, get, rm, rmdir, chdir
 from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps, PkgConfigDeps
 from conan.tools.layout import basic_layout
 import os
+import shutil
 
 required_conan_version = ">=1.54.0"
 
@@ -21,10 +22,12 @@ class LibbpfConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
+        "customXskProg": ["ANY"],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
+        "customXskProg": "",
     }
 
     exports_sources = "config.mk"
@@ -72,8 +75,12 @@ class LibbpfConan(ConanFile):
         autotoolsdeps.generate()
 
     def build(self):
+        if self.options.customXskProg != "":
+            dest = os.path.join(self.source_folder, "lib/libxdp/xsk_def_xdp_prog.c")
+            shutil.copyfile(str(self.options.customXskProg), dest)
+            self.output.info(f"custom prog: {self.options.customXskProg} -> {dest}")
         env = Environment()
-        env.define("BPF_CFLAGS", f'-I{self.dependencies["libbpf"].package_folder}/include')
+        env.define("BPF_CFLAGS", f'-g -I{self.dependencies["libbpf"].package_folder}/include')
         envvars = env.vars(self, scope="build")
         with envvars.apply():
             autotools = Autotools(self)
