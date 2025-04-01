577c925b5f0a9026da7bc5ad1147a4e3f0e0ffda
hoxnox <hoxnox@gmail.com>
2025-04-01 20:08:21 +0300

    ENH: libuuid to conan2

--- recipes/libuuid/all/conanfile.py
+++ recipes/libuuid/all/conanfile.py
@@ -1,8 +1,9 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.tools.layout import basic_layout
+from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps
+from conan.tools.files import copy, get
+from conan.errors import ConanInvalidConfiguration
 import os
-
-required_conan_version = ">=1.33.0"
 
 
 class LibuuidConan(ConanFile):
@@ -12,68 +13,43 @@ class LibuuidConan(ConanFile):
     homepage = "https://sourceforge.net/projects/libuuid/"
     license = "BSD-3-Clause"
     topics = ("conan", "libuuid", "uuid", "unique-id", "unique-identifier")
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
-    exports_sources = "patches/**"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
 
-    _autotools = None
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    def config_options(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
-
     def configure(self):
-        if self.options.shared:
-            del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
-    def validate(self):
-        if self.settings.os == "Windows":
-            raise ConanInvalidConfiguration("libuuid is not supported on Windows")
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
-    def build_requirements(self):
-        self.build_requires("libtool/2.4.6")
+    def validate(self):
+        if self.settings.os != "Linux":
+            raise ConanInvalidConfiguration(f"{self.ref} is only available on Linux")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self)
-        yes_no = lambda v: "yes" if v else "no"
-        configure_args = [
-            "--enable-shared={}".format(yes_no(self.options.shared)),
-            "--enable-static={}".format(yes_no(not self.options.shared)),
-        ]
-        if "x86" in self.settings.arch:
-            self._autotools.flags.append('-mstackrealign')
-        self._autotools.configure(args=configure_args, configure_dir=self._source_subfolder)
-        return self._autotools
+    def generate(self):
+        tc = AutotoolsToolchain(self)
+        tc.configure_args.append("--enable-shared=%s" % ("yes" if self.options.shared else "no"))
+        tc.configure_args.append("--enable-static=%s" % ("no" if self.options.shared else "yes"))
+        tc.generate()
+
+        autotoolsdeps = AutotoolsDeps(self)
+        autotoolsdeps.generate()
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-        with tools.chdir(self._source_subfolder):
-            self.run("{} -fiv".format(tools.get_env("AUTORECONF")), run_environment=True)
-        autotools = self._configure_autotools()
+        autotools = Autotools(self)
+        autotools.configure()
         autotools.make()
 
     def package(self):
-        self.copy("COPYING", dst="licenses", src=self._source_subfolder)
-        autotools = self._configure_autotools()
+        autotools = Autotools(self)
         autotools.install()
-        tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*.la")
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
-        self.cpp_info.names["pkg_config"] = "uuid"
         self.cpp_info.libs = ["uuid"]
-        self.cpp_info.includedirs.append(os.path.join("include", "uuid"))
+
